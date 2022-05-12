8ae879a2481b497a22085e3bb2624064ccc7dda1
moonman369 <mightyayan369@gmail.com>
2022-05-12 19:57:07 +0530

    commit-12.5.22-3

--- contracts/crowdsale.sol
+++ contracts/crowdsale.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../math/SafeMath.sol";
 import "../token/ERC20/SafeERC20.sol";
