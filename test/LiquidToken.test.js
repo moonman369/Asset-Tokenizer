ce26884abbf0d42acc4b52b62384d550d862eac8
moonman369 <mightyayan369@gmail.com>
2022-05-13 01:01:14 +0530

    commit-12.5.22-2

--- test/LiquidToken.test.js
+++ test/LiquidToken.test.js
@@ -14,8 +14,12 @@ const expect = chai.expect;
 contract("LiquidToken", async (accounts) => {
   const [creator, recipient, anotherAccount] = await accounts;
 
+  beforeEach(async () => {
+    this.LQD = await LiquidToken.new(1000000);
+  });
+
   it("mint initial supply in creator's account", async () => {
-    let instance = await LiquidToken.deployed();
+    let instance = this.LQD;
     let totalSupply = await instance.totalSupply();
     //let balance = await instance.balanceOf (creatorAddress);
     // assert.equal (balance.valueOf(), totalSupply.valueOf(), "Balance was not the same")
@@ -31,31 +35,33 @@ contract("LiquidToken", async (accounts) => {
   });
 
   it("should send 50% of the total supply to the crowdsale contract", async () => {
-    let instance1 = await LiquidToken.deployed();
-    let totalSupply = await Number(instance1.totalSupply());
-    let instance2 = await LiquidTokenSale.deployed();
+    let instance1 = this.LQD;
+    let totalSupply = instance1.totalSupply();
 
     expect(
-      instance.balanceOf(LiquidTokenSale.address)
+      instance1.balanceOf(LiquidTokenSale.address)
     ).to.eventually.be.a.bignumber.equal(new BN(totalSupply / 2));
 
-    expect;
+    expect(instance1.balanceOf(creator)).to.eventually.be.a.bignumber.equal(
+      new BN(totalSupply / 2)
+    );
   });
 
   it("is not possible to send an amount greater than the total supply", async () => {
-    let instance = await LiquidToken.deployed();
+    let instance = this.LQD;
     let totalSupply = await instance.totalSupply();
     let creatorBalance = await instance.balanceOf(creator);
-    let sendAmount = Number(creatorBalance) + 1;
+    let sendAmount = 1000001;
 
-    expect(instance.transfer(recipient, sendAmount)).to.eventually.be.rejected;
+    expect(instance.transfer(recipient, new BN(sendAmount))).to.eventually.be
+      .rejected;
     expect(instance.balanceOf(creator)).to.eventually.be.a.bignumber.equal(
       totalSupply
     );
   });
 
   it("is possible to send tokens between accounts", async () => {
-    let instance = await LiquidToken.deployed();
+    let instance = this.LQD;
     const sendAmount = 1;
     let totalSupply = await instance.totalSupply();
     expect(instance.balanceOf(creator)).to.eventually.be.a.bignumber.equal(
