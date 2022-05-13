4da65f87d7e6c15f6c945f6bd3d5992ae3200f3d
moonman369 <mightyayan369@gmail.com>
2022-05-14 01:25:32 +0530

    commit-14.5.22-6 Breaking: Started developing the frontend by modifying the client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,42 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+
+import LiquidToken from "./contracts/LiquidToken.json";
+import LiquidTokenSale from "./contracts/LiquidTokenSale.json";
+import LiquidTokenKYC from "./contracts/LiquidTokenKYC.json";
+
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {};
 
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+      this.web3 = await getWeb3();
 
       // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      this.accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
+      this.networkId = await web3.eth.net.getId();
+      this.LiquidToken = new web3.eth.Contract(
+        LiquidToken.abi,
+        LiquidToken.networks[this.networkId] &&
+          LiquidToken.networks[this.networkId].address
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({}, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract. Check console for details.`
       );
       console.error(error);
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
   };
 
   render() {
