d526b78f5e3da068c6cea8fd04f0139e5c634477
moonman369 <mightyayan369@gmail.com>
2022-05-14 23:12:41 +0530

    commit-14.5.22-15

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+require("dotenv").config({ path: "./.env" });
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.TRUFFLE_HDWALLET_MNEMONIC;
+const accountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +13,16 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+    },
+    ganache_local: {
+      provider: function () {
+        return new HDWalletProvider(
+          mnemonic,
+          "http://127.0.0:7545",
+          accountIndex
+        );
+        network_id: "*";
+      },
     },
   },
   compilers: {
