0956171250c13f19cd020d2610a368d3dc072c89
moonman369 <mightyayan369@gmail.com>
2022-05-15 01:40:06 +0530

    commit-15.5.22-2 increased total supply

--- contracts/liquidtoken.sol
+++ contracts/liquidtoken.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LiquidToken is ERC20 {
+
      constructor (uint256 _initialSupply) ERC20 ("Liquid Token", "LQD") {
-         _mint (_msgSender(), _initialSupply * decimals());
+         _mint (_msgSender(), _initialSupply);
      }
 }
